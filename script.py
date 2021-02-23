926346bc8a660ff0b64469195d9a0861b32bc024
Tiemon-hoi <64529962+Tiemon-hoi@users.noreply.github.com>
2021-02-23 12:18:22 +0100

    Update script.py

--- script.py
+++ script.py
@@ -6,11 +6,7 @@ print("Make sure you don't have two EFI's mounted at the same time")
 print("The script will now check for newer updates...")
 import urllib.request
 import time, os, urllib, sys, stat, json, zipfile, shutil, platform, subprocess
-version ="""{
-    "version": "0.5",
-    "author": "Tijmen",
-    "type": "script"
-}"""
+version = 0.5
 
 path = os.path.realpath(__file__)
 
