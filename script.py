5530817295339460946c3c436348b56e71e1b81c
Tiemon-hoi <64529962+Tiemon-hoi@users.noreply.github.com>
2021-02-24 16:33:21 +0100

    Update script.py

--- script.py
+++ script.py
@@ -127,11 +127,11 @@ for asset in json_data["assets"]:
     shutil.rmtree("/Volumes/EFI/EFI/OC/Kexts/Lilu.kext", ignore_errors=True)
     shutil.copytree("Lilu.kext", "/Volumes/EFI/EFI/OC/Kexts/Lilu.kext")
 if os.path.exists("/Volumes/EFI/EFI/OC/Kexts/CPUFriend.kext"):
- hello_data = urlopen("https://api.github.com/repos/acidanthera/CPUFriend/releases/latest").read()
-json_data = json.loads(hello_data)
-for asset in json_data["assets"]:
+  url_data = urlopen("https://api.github.com/repos/acidanthera/CPUFriend/releases/latest").read()
+  json_data = json.loads(url_data)
+  for asset in json_data["assets"]:
     if "RELEASE" not in asset["name"]:
-        continue
+      continue
     urlcpufriend = asset["browser_download_url"]
     urllib.request.urlretrieve(urlcpufriend, 'CPUFriend.zip')
     with zipfile.ZipFile('CPUFriend.zip', 'r') as zip_ref:
